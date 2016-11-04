a5d92c1a1d956f0be536f0448b354445b2dc61d9
rajstha69 <rajendra.shrestha69@yahoo.com>
2016-11-04 13:54:19 -0700

    Add files via upload

--- JFrame/apples.java
+++ JFrame/apples.java
@@ -1,14 +1,18 @@
 package JavaClass.JFrame;
+
 import javax.swing.JFrame;
 
-public class apples {
-
+class apples{
 	public static void main(String[] args) {
-		
-	tuna bucky = new tuna();
-	bucky.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	bucky.setSize(100,150);
-	bucky.setVisible(true);
+		tuna bucky = new tuna();
+		bucky.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		bucky.setSize(350, 100);
+		bucky.setVisible(true);
 	}
+	
+	
 
+
+	
 }
+
